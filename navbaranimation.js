6cb37a512f9465298006eb11732a462a9af8be51
Mikkermand <Mikkermand@users.github.com>
2020-03-03 15:44:44 +0100

    Navbar animaton scroll

--- navbaranimation.js
+++ navbaranimation.js
@@ -1,7 +1,20 @@
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('nav').addClass('transparent');
-  } else {
-    $('nav').removeClass('transparent');
-  }
-});
+var scrollY = 0;
+var distance = 40;
+var speed = 24;
+function autoScrollTo(el) {
+	var currentY = window.pageYOffset;
+	var targetY = document.getElementById(el).offsetTop;
+	var bodyHeight = document.body.offsetHeight;
+	var yPos = currentY + window.innerHeight;
+	var animator = setTimeout('autoScrollTo(\''+el+'\')',24);
+	if(yPos > bodyHeight){
+		clearTimeout(animator);
+	} else {
+		if(currentY < targetY-distance){
+		    scrollY = currentY+distance;
+		    window.scroll(0, scrollY);
+	    } else {
+		    clearTimeout(animator);
+	    }
+	}
+}
