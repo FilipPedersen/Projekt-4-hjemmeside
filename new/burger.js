bacf050fb6587b9e92e0e931691e0a1f971f8a57
Mikkermand <60429647+Mikkermand@users.noreply.github.com>
2020-03-04 20:19:45 +0100

    Burger menu

--- new/burger.js
+++ new/burger.js
@@ -1,9 +1,14 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
+    const nav = document.querySelector('.nav-link');
+    const navLinks = document.querySelectorAll('.nav-link li');
 
-    burger.addEventListener('click',()=>{
+    burger.addEventListener('click',() => {
       nav.classList.toggle('nav-active');
+    });
+
+    navLinks.forEach((link, index) => {
+      link.style.animation = `navLinkFade 0.5s ease forward ${index / 7 + 2}s`;
     });
 }
 
