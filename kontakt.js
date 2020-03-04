09a0e096f2c73c08180b4471c91c551cb696ca75
fili4193 <60428645+fili4193@users.noreply.github.com>
2020-03-04 10:31:24 +0100

    Javascript error message

--- kontakt.js
+++ kontakt.js
@@ -1,3 +1,18 @@
 function validation(){
-  
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var nummer = document.getElementById("nummer").value;
+  var besked = document.getElementById("besked").value;
+  var error_message = document.getElementById("error_message");
+  var text;
+
+  error_message.style.padding = "10px";
+
+  if(name.length <3){
+    text = "Skriv venligst dit navn";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  return false;
 }
